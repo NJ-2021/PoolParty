b3d0489dda5c8608a8a9f77a924c6138aa14be0f
zlace <zlace0x@gmail.com>
2024-07-14 07:44:33 +0200

    feat: bump wallet salt

--- frontend/components/welcome/index.tsx
+++ frontend/components/welcome/index.tsx
@@ -5,7 +5,7 @@ import { DynamicWidget } from "@dynamic-labs/sdk-react-core";
 export function Welcome() {
     return <div className="relative">
         <div
-            className="relative z-10 flex flex-col justify-center items-center space-y-4 bg-cover bg-no-repeat bg-center p-8 w-full"
+            className="relative z-10 flex flex-col justify-center items-center space-y-4 bg-cover bg-no-repeat bg-center p-8 rounded-lg w-full"
             style={{
                 backgroundImage: `linear-gradient(90deg, rgba(2,0,36,0.7833727240896359) 0%, rgba(2,0,36,0.3744091386554622) 51%, rgba(2,0,36,0.7917760854341737) 100%), url('/bg.png')`,
             }}
